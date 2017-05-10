1fd9573ef9c358108b8665d876634210ccd9bb54
Sadik Messaoudi <sadikano10@gmail.com>
2017-05-10 12:35:42 +0200

    Added GfyCat Api Commands : Search and Trending

--- Modules/CommandModule.cs
+++ Modules/CommandModule.cs
@@ -1,13 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Gfycat;
+
+
 namespace MessaBotCore.Modules
 {
+    [Group("GfyCat")]
     public class CommandModule : ModuleBase
     {
-        [Command("Info"),Summary("Give informations about")]
-        public async Task Info()
+        private Random rnd = new Random();
+        private GfycatClient gfyClient = new GfycatClient("2_Wz_5hd","JLd1JKMHxxjjcuJFqK7FDcKeuZnOepGNE3c52Lapae7RKO9ESnVHEtgrXU7jxZkq");
+
+        [Command("Trending"),Summary("Return a random trending gif from GfyCat")]
+        public async Task Trending(string category=null)
         {
-            await ReplyAsync("Fine");
+            var gifs = new List<Gfy>((await gfyClient.GetTrendingGfysAsync(category)).Content);
+            await ReplyAsync(gifs[rnd.Next(0,gifs.Count)].Url);
+        }
+
+        [Command("Search"),Summary("Return a random gif from GfyCat using search")]
+        public async Task Search([RemainderAttribute]string search)
+        {
+                var gifs = new List<Gfy>((await gfyClient.SearchAsync(search)).Content);
+                await ReplyAsync(gifs[rnd.Next(0,gifs.Count)].Url);
         }
     }
 }
